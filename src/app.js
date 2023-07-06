f17811add6bdb41285f6225c5e573a9da07d567e
JParedesArtica <126907318+JParedesArtica@users.noreply.github.com>
2023-07-06 02:32:35 +0000

    excusas

--- src/app.js
+++ src/app.js
@@ -7,30 +7,30 @@ window.onload = () => {
   document.querySelector("#btn").addEventListener("click", () => {
     document.querySelector("#excuse").innerHTML = generateExcuse();
   });
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "His Turtle", "Mi bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let actIndex = Math.floor(Math.random() * action.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+
+    return who[whoIndex].concat(
+      " ",
+      action[actIndex],
+      " ",
+      what[whatIndex],
+      " ",
+      when[whenIndex]
+    );
+  }
 };
-function generateExcuse() {
-  let who = ["The dog", "My grandma", "His Turtle", "Mi bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let actIndex = Math.floor(Math.random() * action.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let whenIndex = Math.floor(Math.random() * when.length);
-
-  return who[whoIndex].concat(
-    " ",
-    action[actIndex],
-    " ",
-    what[whatIndex],
-    " ",
-    when[whenIndex]
-  );
-}
